6dccb1a69f1f3fa0890a7f4189a9e9a717a78047
Your Name <sy9843478094>
2022-04-21 02:17:23 +0545

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs')
 console.log(__dirname)
 console.log(path.join(__dirname,'../public'))
 const app = express()
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
 const partialsPath = path.join(__dirname,'../templates/partials')
@@ -40,7 +41,7 @@ app.get('/weather', (req, res)=>{
         ])
         })
 
-app.listen(3000, ()=>{
-console.log('server is up on port 3000')
+app.listen(port, ()=>{
+console.log('server is up on port'+ port)
 
 })
