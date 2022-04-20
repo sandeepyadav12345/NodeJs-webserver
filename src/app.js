24822be4a6a51d4902824e47c57b72a84a114b49
Your Name <sy9843478094>
2022-04-21 00:43:37 +0545

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+console.log(__dirname)
+console.log(path.join(__dirname,'../public'))
+const app = express()
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req,res)=>{
+
+        res.render('index', {
+                title:'Photo',
+                name: 'Sandeep'
+        })
+        
+
+
+})
+
+
+
+
+app.get('/weather', (req, res)=>{
+            res.send([
+                {
+                location:'kathmandu',
+                forecast: '25 degree'
+                },
+        
+                {
+                location:'birgunj',
+                forecast:'50 degree'
+                }
+        ])
+        })
+
+app.listen(3000, ()=>{
+console.log('server is up on port 3000')
+
+})
